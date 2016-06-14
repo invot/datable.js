eb6202d4ba00d5899d8943fdae0c48169496205f
invot <invot@users.noreply.github.com>
2016-06-14 10:17:33 -0500

    Better validation rules & bugfixes

--- datable.js
+++ datable.js
@@ -1,5 +1,5 @@
 // datable.js by invot
-// version 0.1.5
+// version 0.2.0
 
 /* global $, jQuery */
 
@@ -8,6 +8,15 @@ if(!String.prototype.endsWith){String.prototype.endsWith=function(searchString,p
 position-=searchString.length;var lastIndex=subjectString.indexOf(searchString,position);return lastIndex!==-1&&lastIndex===position;};}
 
 function toObject(t,n){for(var r={},e=0;e<t.length;e++)r[t[e]]=n[e];return r;}
+
+function isDate(y, m, d) {
+    var a = new Date(y, m-1, d);
+    if (a.getFullYear() == y && a.getMonth() == m-1 && a.getDate() == d) { 
+    	return true; 
+    } else {
+    	return false;
+    }
+}
 
 (function ( $ ) {    
 $.fn.datable = function() {
@@ -33,7 +42,7 @@ $.fn.datable = function() {
         t.attr('maxlength', phld.length);
         t.attr('placeholder', phld).val();
         t.on('keydown', function(e){
-            if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105) && $.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) === -1) { e.preventDefault(); }
+            if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105) && $.inArray(e.keyCode, [46, 8, 9, 27, 13]) === -1) { e.preventDefault(); }
             if(e.keyCode == 8) {
                 if ( t.val().endsWith(div) ) {
                     t.val( t.val().replace(new RegExp(div + '$'),'') );
@@ -56,19 +65,12 @@ $.fn.datable = function() {
         });  
         if(era) {
             var st,er;
-            if (!sta) {
-                var d = new Date();
-                st = Math.round( d.getTime()/1000);
-                sta = d.getFullYear() + "," + (d.getMonth()+1) +"," + d.getDate();
-            } else {
-                st = Math.round(new Date( sta.replace(',','/') ).getTime()/1000);
-            }
-            var sArr = sta.split(','),
-                eArr = era.split(','),
-                y = (parseInt(sArr[0])+parseInt(eArr[0])) +'/'+
-                (parseInt(sArr[1])+parseInt(eArr[1])) +'/'+
-                (parseInt(sArr[2])+parseInt(eArr[2]));
-            er = Math.round(new Date(y).getTime()/1000);    
+            sta ? st = new Date(sta) : st = new Date();
+            var e = era.split(','),
+                day = parseInt(st.getDate()) + parseInt(e[2]),
+                month = parseInt(st.getMonth()) + parseInt(e[1]),
+                year = parseInt(st.getFullYear()) + parseInt(e[0]);  
+            er = new Date(year,month,day);   
         }
 
         // BOOTSTRAP-FRIENDLY VALIDATION 
@@ -76,25 +78,31 @@ $.fn.datable = function() {
             var dat = t.val().split(div),
                 arr = toObject(nrr,dat),
                 err = 0;
-            if(arr.mm > 12 || arr.mm < 1 ) {
-                err++;
+            if (arr.yy) {
+                arr.yyyy = 2000 + parseInt(arr.yy);
             }
-            if(arr.dd > 31 || arr.dd < 1) {
-                err++;
+            if (!arr.dd) {
+                arr.dd = 01;
             }
-            if(arr.yyyy > 3000 || arr.yyyy < 1000) {
-                err++;
-            }
-            if(era) { 
-                var tv = Math.round(new Date(arr.yyyy+'/'+arr.mm+'/'+arr.dd).getTime()/1000);
-                if (!isNaN(tv)){
+            console.log(arr.dd);
+            console.log(arr.mm);
+            console.log(arr.yyyy);
+
+            if ( isDate(arr.yyyy,arr.mm,arr.dd) ) {
+                console.log("it's a date!");
+                var tv = new Date(arr.yyyy, arr.mm, arr.dd);
+                console.log("tv:" + tv.toString());
+                if(era) { 
                     if (tv > st && tv >= er) {
                         err++;
                     } else if (tv < st && tv <= er) {
                         err++;   
                     }
                 }
+            } else {
+                err++;
             }
+           
             if( t.val().length !== 0 && err ) {
                 t.addClass('error').closest('.control-group').addClass('has-error');
             } else {
